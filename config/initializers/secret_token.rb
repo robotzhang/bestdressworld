3aa3d66574edee1b1bbc8e0564ce505101373f78
robotzhang <445679586@qq.com>
2013-04-06 18:14:01 +0800

    bestdressworld create!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bestdressworld::Application.config.secret_token = 'e3a8f057b1ed840dc0b90c27d2e3c2fb7457aa6afb3695c4d24994f8f8d91f827995c3204327e66d63c7e5a3c01ddc5c8d6aafde2d7a07d921261b8085158037'

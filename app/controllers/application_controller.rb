1e688f1d30f5cebd6ced871f90e74360892b5860
robotzhang <445679586@qq.com>
2013-04-08 16:35:34 +0800

    后台框架完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+#coding=utf-8
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def homepage
@@ -6,5 +7,24 @@ class ApplicationController < ActionController::Base
 
   def admin
     redirect_to admin_products_url
+  end
+
+  # 登录验证
+  def authenticated(options)
+    default ={
+        :role => '',
+        :alert => '请先登陆'
+    }
+    options = default.merge(options)
+    if  current_user.nil? || (!options[:role].empty? && current_user.role != options[:role])
+      redirect_to login_url, :alert => options[:alert]
+      return false
+    end
+    true
+  end
+
+  # 当前登录用户
+  def current_user
+    session[:user]
   end
 end

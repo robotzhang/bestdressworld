75ac15fdcd1cedb638eff4b5d176102866d26875
robotzhang <445679586@qq.com>
2013-04-14 18:19:36 +0800

    已经抓取过的产品不再抓取

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -7,6 +7,7 @@ class Admin::ProductsController < ApplicationController
 
   def amazon
     asin = params[:asin]
+    return redirect_to({:action => :index}, :flash => { :error => 'asin: '+asin+' 已经抓取！' }) unless Product.find_by_asin(asin).blank?
     return redirect_to({:action => :index}, :flash => { :error => '要抓取的asin不能为空！' }) if asin.blank?
     @product = Product.get_amazon(asin)
   end

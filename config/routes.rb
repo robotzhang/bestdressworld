fd6edaaa7681d6bfd9aa02561b9c98fb951c7745
robotzhang <445679586@qq.com>
2013-04-15 16:14:42 +0800

    产品列表页瀑布流展示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Bestdressworld::Application.routes.draw do
   root :to => 'application#homepage'
   get 'admin' => 'application#admin'
+  resources :products
   namespace :admin do |admin|
     resources :products
     get 'amazon' => 'products#amazon'

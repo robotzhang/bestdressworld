041457fe5951c15301eaee44c9c5f5e2e67d3cf8
robotzhang <445679586@qq.com>
2013-04-22 21:06:16 +0800

    图片未载入前div高度占位置

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,8 +1,7 @@
 module ProductsHelper
-  def render_image(image, max_width=225)
+  def image_height_width(image, max_width=225)
     width = image.width < max_width ? image.width : max_width
     height = image.height * width / image.width
-
-    "<img style='width:#{width};height:#{height};' src='#{image.url}'/>".html_safe
+    {:width => width, :height => height}
   end
 end

3e455cfbdb6578f920a9b0e61e611e8fe31d3541
robotzhang <445679586@qq.com>
2013-04-22 20:52:01 +0800

    图片显示不确切的问题

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,8 @@
 module ProductsHelper
+  def render_image(image, max_width=225)
+    width = image.width < max_width ? image.width : max_width
+    height = image.height * width / image.width
+
+    "<img style='width:#{width};height:#{height};' src='#{image.url}'/>".html_safe
+  end
 end

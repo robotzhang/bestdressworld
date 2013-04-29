d4fe7298c81e0a9237138e19126d60552d41e3f5
robotzhang <445679586@qq.com>
2013-04-29 20:00:22 +0800

    分页seo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Bestdressworld::Application.routes.draw do
   root :to => 'application#homepage'
   get 'admin' => 'application#admin'
   get 'products/:seo_url' => 'products#show'
-  resources :products
+  resources :products do
+    get 'page/:page', :action => :index, :on => :collection
+  end
   namespace :admin do |admin|
     get 'products/amazon' => 'products#amazon'
     post 'products/create_category' => 'products#create_category'

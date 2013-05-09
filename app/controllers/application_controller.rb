4fdcd20c1b73067f0ad92efbec85ed74861ac51f
robotzhang <445679586@qq.com>
2013-05-09 21:29:21 +0800

    首页加上最新产品列表

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def homepage
+    @products = Product.order("id DESC").includes([:images]).limit(10).all
     render :template => 'coming', :layout => false
   end
 

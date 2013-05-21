3c3f969c86dc1f854dbdc1a66b1e0af09161c778
robotzhang <445679586@qq.com>
2013-05-21 17:57:35 +0800

    google 第三方登录

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,8 @@ class User < ActiveRecord::Base
 
   # 通过第三方查找或者创建用户
   def self.find_or_create_from_auth_hash(auth)
-    user = User.new({:sns_uid => auth.uid, :sns_provider => auth.provider, :nickname => auth.info.name, :image => auth.info.image})
+    user = User.new({:sns_uid => auth.uid, :sns_provider => auth.provider, :nickname => auth.info.nickname, :image => auth.info.image})
+    user.nickname = auth.info.name if user.nickname.blank?
     user.email = auth.info.email if auth.info.email
     user_db = User.where({:sns_uid => user.sns_uid, :sns_provider => user.sns_provider}).first
     return user_db unless user_db.blank?
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :twitter, 'wfv1ezxG7IfnBxPR87S0wg', '0t6g3Svs2FLktHZ3yEcywUVzFzz6ufJxGR60uHMUaY'
   provider :facebook, '371470182974626', '5eadc2802d9f814d1d6d7af2b6239baa'
+  provider :google, '79816562973.apps.googleusercontent.com', '_QUXnZWXfB59AFTFfydAxbnt'
 end

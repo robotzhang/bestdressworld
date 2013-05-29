29b457bb350aa86a64f0504d0435f7934b73e3ba
robotzhang <445679586@qq.com>
2013-05-29 17:57:15 +0800

    my shares limit to 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   end
 
   def shares
-    @products = Product.desc.where({:user_id => @user.id}).page(params[:page]).per(10).decorate
+    @products = Product.desc.where({:user_id => @user.id}).page(params[:page]).per(8).decorate
   end
 
   protected

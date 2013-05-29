2ade664769eb776421de5db4924d02eb8bab3b6a
robotzhang <445679586@qq.com>
2013-05-29 17:11:40 +0800

    seo description和keywords判断错误

--- app/decorators/product_decorator.rb
+++ app/decorators/product_decorator.rb
@@ -6,12 +6,12 @@ class ProductDecorator < Draper::Decorator
   end
 
   def seo_title
-    return object.seo_title if !object.seo_title.blank?
+    return object.seo_title unless object.seo_title.blank?
     name.downcase+" - #{object.currency} #{object.currency_symbol} #{object.price.to_s}"
   end
 
   def seo_keywords
-    return object.seo_keywords if !object.seo_keywords
+    return object.seo_keywords unless object.seo_keywords.blank?
     keywords = ""
     keywords += object.categories.map {|c| c.name}.join(' ') if !object.categories.blank?
     keywords += object.options.map {|o| o.name_en }.uniq.join(' ') if !object.options.blank?
@@ -19,7 +19,7 @@ class ProductDecorator < Draper::Decorator
   end
 
   def seo_description(domain="bestdressworld.com")
-    return object.seo_description if !object.seo_descriptions
+    return object.seo_descriptions unless object.seo_descriptions.blank?
     prefix = seo_keywords
     prefix += prefix+", "if !prefix.blank?
     "find the best #{prefix}#{name.downcase} in the world from #{domain}, buy from #{object.from_site}! "

5cc7836114d82f89f88a2501cb48ee2af36a7214
robotzhang <445679586@qq.com>
2013-05-31 18:33:44 +0800

    不请求facebook的生日

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :twitter, APP_CONFIG["twitter_id"], APP_CONFIG['twitter_secret']
   provider :facebook, APP_CONFIG["facebook_id"], APP_CONFIG['facebook_secret'],
-           :image_size => "large", :scope => "email,user_birthday"
+           :image_size => "large", :scope => "email"
   provider :google, APP_CONFIG["google_id"], APP_CONFIG['google_secret']
 end
